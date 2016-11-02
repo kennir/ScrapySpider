af4ad6bf4cc059f6f9792f8a6831956ff220b221
张靖宇 <kennir@gmail.com>
2016-11-02 14:12:28 +0800

    通过管道移除非法字符

--- spider/pipelines.py
+++ spider/pipelines.py
@@ -5,32 +5,14 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
-import sqlite3
-from os import path
-from scrapy import signals
-from scrapy.xlib.pydispatch import dispatcher
 
-class HomeSqlite3Pipeline(object):
-
-    def __init__(self):
-        self.conn = None
-        dispatcher.connect(self.initialize, signals.engine_started)
-        dispatcher.connect(self.finalize, signals.engine_stopped)
-
+class HomeRemoveInvalidCharPipeline(object):
     def process_item(self, item, spider):
+        if item['title']:
+            item['title'] = item['title'].replace('\xb2', '')
+        if item['area']:
+            item['area'] = item['area'].replace('\xb2', '')
+        if item['price_per_sqm']:
+            item['price_per_sqm'] = item['price_per_sqm'].replace('\xb2', '')
 
         return item
-
-    def initialize(self):
-        print('------------ INITIALIZE --------')
-        if path.exists(self.filename):
-            self.conn = sqlite3.connect(self.filename)
-        else:
-            self.conn = self.create_table(self.filename)
-
-    def finalize(self):
-        print('------------ finalize --------')
-        if self.conn is not None:
-            self.conn.commit()
-            self.conn.close()
-            self.conn = None
